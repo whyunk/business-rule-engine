fc1f440750b036f77475414fbc191bdaa95194f9
Joseph Kim <whyunk79@gmail.com>
2023-11-09 13:43:58 +0900

    feat: default api test code successful

--- src/main/java/BusinessRuleEngine.java
+++ src/main/java/BusinessRuleEngine.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BusinessRuleEngine {
 
+    private final List<Action> actions;
+
+    public BusinessRuleEngine() {
+        this.actions = new ArrayList<>();
+    }
+
     public void addAction(final Action action) {
-        throw new UnsupportedOperationException();
+        actions.add(action);
     }
 
     public int count() {
-        throw new UnsupportedOperationException();
+        return actions.size();
     }
 
     public void run() {
