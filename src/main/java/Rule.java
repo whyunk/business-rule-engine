61fd828a0d8edac0b775ca57c6859b1ddadcd28d
Joseph Kim <whyunk79@gmail.com>
2023-11-09 16:15:27 +0900

    feat: separate interface methods for isp and Rule

---
+++ src/main/java/Rule.java
@@ -0,0 +1,22 @@
+@FunctionalInterface
+public interface Rule {
+    void perform(Facts facts);
+}
+
+class DefaultRule implements Rule {
+
+    private final Condition condition;
+    private final Action action;
+
+    public DefaultRule(Condition condition, Action action) {
+        this.condition = condition;
+        this.action = action;
+    }
+
+    @Override
+    public void perform(Facts facts) {
+        if (condition.evaluate(facts)) {
+            action.execute(facts);
+        }
+    }
+}
