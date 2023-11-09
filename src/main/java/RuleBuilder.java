4124e6361760352740f99c3f18175d77e3acaf7a
Joseph Kim <whyunk79@gmail.com>
2023-11-09 16:28:11 +0900

    refactor: rule builder

--- src/main/java/RuleBuilder.java
+++ src/main/java/RuleBuilder.java
@@ -1,18 +1,21 @@
 public class RuleBuilder {
     private Condition condition;
-    private Action action;
 
-    public RuleBuilder when(final Condition condition) {
+    private RuleBuilder(final Condition condition) {
         this.condition = condition;
-        return this;
     }
 
-    public RuleBuilder then(final Action action) {
-        this.action = action;
-        return this;
+    public static RuleBuilder when(final Condition condition) {
+        return new RuleBuilder(condition);
     }
 
+    public Rule then(final Action action) {
+        return new DefaultRule(condition, action);
+    }
+
+    /*
     public Rule createRule() {
         return new DefaultRule(condition,action);
     }
+    */
 }
